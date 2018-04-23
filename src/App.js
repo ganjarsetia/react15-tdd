75dcf28594bdac7311434f3317b238f8a1f9885d
Ganjar Setia <ganjar_setia@ymail.com>
2018-04-23 16:55:45 +0700

    Integrate product list to main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ProductList from './components/ProductList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [
+                {id: 1, name: 'AirMax 90', brand: 'Nike'},
+                {id: 2, name: 'Yeezy', brand: 'Adidas'},
+                {id: 3, name: 'Classic', brand: 'Reeebok'},
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My Product Store</h1>
+                <ProductList products={this.state.products}/>
+            </div>
+        );
+    }
 }
 
 export default App;
